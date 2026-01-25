4e6ff9d66369d5608d7a932fa229884281d182c9
akshat-loya <loya.akshat@gmail.com>
2026-01-25 13:02:41 +0530

    Giving the engine openGL context using glfw

--- Engine/include/core/Engine.h
+++ Engine/include/core/Engine.h
@@ -1,8 +1,16 @@
+#define GL_SILENCE_DEPRECATION
+
 #include <iostream>
+#include <OpenGL/gl.h>
+#include <OpenGL/gl3.h>
+#include <GLFW/glfw3.h>
 
 class Engine
 {
 public:
-    Engine() { std::cout << "Engine contructed..." << std::endl; };
+
+    Engine()  { std::cout << "Engine contructed..." << std::endl; };
     ~Engine() { std::cout << "Engine destructed..." << std::endl; };
+
+    int initialise();
 };
--- Engine/src/core/Engine.cpp
+++ Engine/src/core/Engine.cpp
@@ -1,1 +1,56 @@
 #include "Engine.h"
+
+int Engine::initialise()
+/* ===================
+ *
+ * For now, just to test that OpenGL works
+ */
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* To print openGL version
+    * std::cout << glGetString(GL_VERSION) << std::endl;
+    */
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Modern OpenGL attempt at drawing a triangle */
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        /* Legacy OpenGL for a triangle */
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-1.0f, -1.0f);
+        glVertex2f( 0.0f,  1.0f);
+        glVertex2f( 1.0f, -1.0f);
+        glEnd();
+    
+        
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
